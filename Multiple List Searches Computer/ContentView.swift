5025c8d6988d8a22af4ac0b7d12320e76d8cffdf
ethamoos <amosdeane@gmail.com>
2024-03-07 21:26:51 +0000

    Basic version working

--- Multiple List Searches Computer/ContentView.swift
+++ Multiple List Searches Computer/ContentView.swift
@@ -8,17 +8,82 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    let computers = ["Big Mac", "Mac Mini", "iBook" ]
+    
+    @State var searchText = ""
+    @State var selection = Set<String>()
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        
+        
+        VStack(alignment: .leading) {
+
+            Text("Computers").bold()
+            List(searchResults, id: \.self, selection: $selection) { computer in
+                
+                HStack {
+                    Image(systemName: "apple.logo")
+                    Text(computer ).font(.system(size: 12.0)).foregroundColor(.blue)
+                }
+                .foregroundColor(.blue)
+            }
+            .searchable(text: $searchText)
         }
         .padding()
+
+        SecondView()
+
+    }
+    
+    var searchResults: [String] {
+        if searchText.isEmpty {
+            print("Policy search is empty")
+            return computers
+        } else {
+            print("Computer search added - item is:\(searchText)")
+            return computers.filter { $0.contains(searchText) }
+        }
     }
 }
 
-#Preview {
-    ContentView()
+
+
+struct SecondView: View {
+    
+    let policies = ["Do hoovering", "Auto-Sandwich", "Water Plants"]
+    @State var searchText = ""
+    @State var selection = Set<String>()
+        
+    var body: some View {
+        
+        VStack(alignment: .leading) {
+            
+            Text("Policies").bold()
+
+            List(searchResults, id: \.self, selection: $selection) { policy in
+                
+                HStack {
+                    Image(systemName: "house")
+                    Text(policy ).font(.system(size: 12.0)).foregroundColor(.blue)
+                }
+                .foregroundColor(.blue)
+            }
+            .searchable(text: $searchText)
+        }
+        .padding()
+    }
+    
+    var searchResults: [String] {
+        if searchText.isEmpty {
+            print("Policy search  is empty")
+            return policies
+        } else {
+            print("Policy search Added")
+            return policies.filter { $0.contains(searchText) }
+        }
+    }
 }
+
+
+
