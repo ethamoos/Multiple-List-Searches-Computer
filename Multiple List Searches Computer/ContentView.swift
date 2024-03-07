66b00dad7c98862414841eef1368a15c92d7869b
ethamoos <amosdeane@gmail.com>
2024-03-07 20:42:51 +0000

    Initial Commit

---
+++ Multiple List Searches Computer/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Multiple List Searches Computer
+//
+//  Created by Amos Deane on 07/03/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Multiple List Searches Computer/Multiple_List_Searches_ComputerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Multiple_List_Searches_ComputerApp.swift
+//  Multiple List Searches Computer
+//
+//  Created by Amos Deane on 07/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct Multiple_List_Searches_ComputerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
